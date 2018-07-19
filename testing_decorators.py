4fdc20928bda02875464af76c8b6c81abccf5f9e
cssrumi <33814533+cssrumi@users.noreply.github.com>
2018-07-19 12:45:51 +0200

    Create testing_decorators.py

---
+++ testing_decorators.py
@@ -0,0 +1,20 @@
+from time import clock
+
+def print_generator(func):
+    def wrapper(*args, **kwargs):
+        for rv in func(*args, **kwargs):
+            print(rv)
+
+    return wrapper
+
+
+def new_timer(func):
+    def wrapper(*args, **kwargs):
+        before = clock()
+        rv = func(*args, **kwargs)
+        after = clock()
+        # print(func.__name__, 'elapsed', after - before)
+        print('%s elapsed %.7f' % (func.__name__, after - before))
+        return rv
+
+    return wrapper
